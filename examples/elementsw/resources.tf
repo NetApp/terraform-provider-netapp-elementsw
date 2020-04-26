afbe719b16b2c1f510f474037677791a6e5153dd
scaleoutSean <scaleoutsean@users.noreply.github.com>
2020-04-26 23:24:20 +0800

    Fix example for TF v0.12.24 and Element v11.7

--- examples/elementsw/resources.tf
+++ examples/elementsw/resources.tf
@@ -1,33 +1,33 @@
 # Specify ElementSW resources
-resource "elementsw_account" "test-account" {
-    provider = "netapp-elementsw"
-    username = "test Account"
+resource "elementsw_account" test-account {
+    provider = netapp-elementsw
+    username = "testAccount"
 }
 
-resource "elementsw_volume" "test-volume" {
-    provider = "netapp-elementsw"
-    name = "test-Volume-${count.index}"
-    account = "${elementsw_account.test-account.id}"
-    total_size = "${var.total_size[count.index]}"
-    enable512e = true
-    min_iops = 50
-    max_iops = 10000
-    burst_iops = 10000
-
+resource "elementsw_volume" test-volume {
     # Create N instances
-    count = "${length(var.total_size)}"
+    count = length(var.volume_size_list)
+    provider = netapp-elementsw
+    name = "${var.volume_name}-${count.index}"
+
+    account = elementsw_account.test-account.id
+    total_size = var.volume_size_list[count.index]
+    enable512e = true
+    min_iops = 100
+    max_iops = 500
+    burst_iops = 1000
 }
 
-resource "elementsw_volume_access_group" "test-group" {
-    provider = "netapp-elementsw"
-    name = "test-Group"
-    volumes = "${elementsw_volume.test-volume.*.id}"
+resource "elementsw_volume_access_group" test-group {
+    provider = netapp-elementsw
+    name = "testGroup"
+    volumes = elementsw_volume.test-volume.*.id
 }
 
-resource "elementsw_initiator" "test-initiator" {
-    provider = "netapp-elementsw"
+resource "elementsw_initiator" test-initiator {
+    provider = netapp-elementsw
     name = "iqn.1998-01.com.vmware:test-es65-7f17a50c"
-    alias = "test EUI Cluster"
-    volume_access_group_id = "${elementsw_volume_access_group.test-group.id}"
-    iqns = "${elementsw_volume.test-volume.*.iqn}"
+    alias = "testVAG"
+    volume_access_group_id = elementsw_volume_access_group.test-group.id
+    iqns = elementsw_volume.test-volume.*.iqn
 }
